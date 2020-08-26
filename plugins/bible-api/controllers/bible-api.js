c0fec02fb98d9b2586863179e38fdcf67b19f6fb
Carlos Sosa <cesosag@gmail.com>
2020-08-26 00:18:45 -0400

    Add ability to store the API Key in the plugin's settings

--- plugins/bible-api/controllers/bible-api.js
+++ plugins/bible-api/controllers/bible-api.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const setStore = (strapi) => {
+  return strapi.store({
+    environment: strapi.config.environment,
+    type: 'plugin',
+    name: 'bible-api'
+  });
+}
+
 /**
  * bible-api.js controller
  *
@@ -21,5 +29,42 @@ module.exports = {
     ctx.send({
       message: 'ok'
     });
+  },
+
+  updateSettings: async (ctx) => {
+    const { user } = ctx.state;
+    const { apiKey } = ctx.request.body;
+    
+    if (user.roles[0].id != 1) {
+      ctx.unauthorized('Only administrators can perform this action');
+    }
+    
+    if (!apiKey) {
+      ctx.throw(400, 'Please provide an API Key')
+    }
+    
+    const pluginStore = setStore(strapi);
+    
+    const result = await pluginStore.set({
+      key: 'apiKey',
+      value: apiKey
+    });
+
+    ctx.send({ result });
+  },
+  
+  getSettings: async (ctx) => {
+    const { user } = ctx.state;
+    if (user.roles[0].id != 1) {
+      ctx.unauthorized('Only administrators can perform this action');
+    }
+    
+    const pluginStore = setStore(strapi);
+    
+    const apiKey = await pluginStore.get({ key: 'apiKey' });
+    
+    ctx.send({
+      apiKey: apiKey ? apiKey : ''
+    })
   }
 };
