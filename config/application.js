3598a54605d970c75558e4c9370c29969107396b
Carlos Sosa <cesosag@gmail.com>
2020-10-08 23:44:04 -0400

    Replace contact address

--- config/application.js
+++ config/application.js
@@ -1,6 +1,6 @@
 module.exports = {
   contact: {
-    email: 'sanysidrosda2020@gmail.com',
+    email: 'contacto@sanysidrosda.com',
     name: 'Iglesia Advetista Hispana de San Ysidro',
     subject: 'Nueva solicitud de contacto en sanysidrosda.com',
     template_id: 'd-c8cfccab944643998f7a94a0d7750f1e',
