bcb89938eda8e84156df2d98d8f6c647f1c95f25
Carlos Sosa <cesosag@gmail.com>
2020-10-25 14:38:06 -0400

    Replace logo and 2 basic colors

---
+++ admin/src/themes/index.js
@@ -0,0 +1,15 @@
+import colors from './colors';
+import fontWeights from './fontWeights';
+import sizes from './sizes';
+import Logo from '../assets/images/SY-logo.svg';
+
+const theme = {
+  main: {
+    colors,
+    fontWeights,
+    sizes,
+    logo: Logo,
+  },
+};
+
+export default theme;
