f49910a0256b82ac3079f761aafdf1d557e6b42e
Carlos Sosa <cesosag@gmail.com>
2020-10-25 14:38:06 -0400

    Replace logo and 2 basic colors

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoStrapi from '../../../../assets/images/SY-logo-dark.svg';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoStrapi} alt="San Ysidro SDA logo" />;
+
+export default Logo;
